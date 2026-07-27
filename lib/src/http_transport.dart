d0da3d128b6a747c692ec989e1455dd47ee20920
visorcraft <thomas@visorcraft.com>
2026-07-27 12:18:23 -0500

    release 0.64.13
    Bump client version and MongrelDB engine/Kit references to 0.64.13.

--- lib/src/http_transport.dart
+++ lib/src/http_transport.dart
@@ -58,7 +58,7 @@ class HttpTransport {
     }
     client = HttpClient()
       ..idleTimeout = idleTimeout
-      ..userAgent = 'mongreldb-dart/0.64.12';
+      ..userAgent = 'mongreldb-dart/0.64.13';
     _client = client;
     return client;
   }
